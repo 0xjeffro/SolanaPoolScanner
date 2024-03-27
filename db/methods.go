cfb595d11ef4279a352bdfebb607fc83e9118203
Jeffro <00002@protonmail.com>
2024-03-28 03:51:16 +0800

    bug fixed

--- db/methods.go
+++ db/methods.go
@@ -2,10 +2,12 @@ package db
 
 import (
 	"SolanaPoolScanner/types"
+	"encoding/json"
 	"go.uber.org/zap"
+	"gorm.io/gorm/clause"
 )
 
-func Insert(rep types.Response, jsonData string) {
+func Insert(rep types.Response) {
 	rows := make([]Trade, 0)
 	for _, item := range rep.Data.Items {
 		txHash := item.TxHash
@@ -14,33 +16,20 @@ func Insert(rep types.Response, jsonData string) {
 		uiChangeAmount := 0.0
 		blockUnixTime := item.BlockUnixTime
 
-		if item.Side == "sell" {
-			if item.Base.Price != nil {
-				estPrice = *item.BasePrice
-			} else if item.Base.NearestPrice != nil {
-				estPrice = *item.Base.NearestPrice
-			} else if item.Quote.Price != nil || item.Quote.NearestPrice != nil {
-				if item.Quote.Price != nil {
-					estPrice = *item.Quote.Price * item.Quote.UiAmount / item.Base.UiAmount
-				} else {
-					estPrice = *item.Quote.NearestPrice * item.Quote.UiAmount / item.Base.UiAmount
-				}
-			}
-			uiChangeAmount = item.Base.UiChangeAmount
-		} else {
+		jsonItem, _ := json.Marshal(item)
+
+		if item.Base.Price != nil {
+			estPrice = *item.BasePrice
+		} else if item.Base.NearestPrice != nil {
+			estPrice = *item.Base.NearestPrice
+		} else if item.Quote.Price != nil || item.Quote.NearestPrice != nil {
 			if item.Quote.Price != nil {
-				estPrice = *item.QuotePrice
-			} else if item.Quote.NearestPrice != nil {
-				estPrice = *item.Quote.NearestPrice
-			} else if item.Base.Price != nil || item.Base.NearestPrice != nil {
-				if item.Base.Price != nil {
-					estPrice = *item.Base.Price * item.Base.UiAmount / item.Quote.UiAmount
-				} else {
-					estPrice = *item.Base.NearestPrice * item.Base.UiAmount / item.Quote.UiAmount
-				}
+				estPrice = *item.Quote.Price * item.Quote.UiAmount / item.Base.UiAmount
+			} else {
+				estPrice = *item.Quote.NearestPrice * item.Quote.UiAmount / item.Base.UiAmount
 			}
-			uiChangeAmount = item.Quote.UiChangeAmount
 		}
+		uiChangeAmount = item.Base.UiChangeAmount
 
 		row := Trade{
 			TxHash:         txHash,
@@ -48,7 +37,7 @@ func Insert(rep types.Response, jsonData string) {
 			EstPrice:       estPrice,
 			UiChangeAmount: uiChangeAmount,
 			BlockUnixTime:  blockUnixTime,
-			Data:           jsonData,
+			Data:           string(jsonItem),
 		}
 		rows = append(rows, row)
 	}
@@ -56,7 +45,8 @@ func Insert(rep types.Response, jsonData string) {
 	// Insert rows into database
 	conn := GetConnection()
 	if len(rows) != 0 {
-		err := conn.Create(&rows).Error
+		err := conn.Clauses(clause.OnConflict{DoNothing: true}).Create(&rows).Error
+		//err := conn.Create(&rows).Error
 		if err != nil {
 			zap.S().Error("Error inserting rows: ", err)
 		}
